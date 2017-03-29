3b8790c9ea0e0dd64f01289aacbb41bf9b2ce3a2
Rachel Kerner <rachel.kerner@gmail.com>
2017-03-29 15:49:10 -0500

    Fix bug in line 28, = instead of ==

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,38 +1,55 @@
 puts "What is your name?"
 name = gets.chomp
 
-puts "How old are you?"
-age = gets.chomp
+if name == "Drake Cula" || name == "Tu Fang"
+  vampirename = true
+else
+  vampirename = false
+end
+
+puts"How old are you?"
+age = gets.chomp.to_i
 
 puts "What year were you born?"
-birthyear = gets.chomp
+birthyear = gets.chomp.to_i
+
+puts realage = 2017 - birthyear
+
+if age == realage
+  correct_age = true
+else
+  correct_age = false 
+end
+
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
 wantgarlic = gets.chomp
 
-puts "Would you like to enroll in the company's health insurance?"
-healthinsurance = gets.chomp
-
-def is_age_correct
-    if (2017 - birthtear) == age
-    correct_age = true
-    else 
-    correct_age = false
-    end
+if wantgarlic == "yes"
+  wantgarlic = true 
+else
+  wantgarlic = false
 end
 
-case is_a_vampire?
-    when correct_age == true
-        && when wantgarlic == "yes" 
-        && when healthinsurance == "yes"
-        puts "Probably not a vampire."
-    when correct_age == false
-        && when wantgarlic == "no",
-        && when healthinsurance == "no"
-        puts "Almost certainly a vampire"
-    when name == "Drake Cula"
-        || name == "Tu Fang"
-        puts "Definitely a vampire"
-    else 
-        puts "Results inconclusive."
+
+puts "Would you like to enroll in the company's health insurance? (yes/no)"
+healthinsurance = gets.chomp
+
+if healthinsurance == "yes"
+  healthinsurance = true 
+else 
+  healthinsurance = false 
+end 
+
+
+if vampirename
+  puts "Definitely a vampire."
+elsif !correct_age && !wantgarlic && !healthinsurance
+  puts "Almost certainly a vampire."
+elsif correct_age && wantgarlic || healthinsurance
+  puts "Probably not a vampire."
+elsif !correct_age && !wantgarlic || !healthinsurance
+  puts "Probably a vampire"
+else 
+  puts "Results inconclusive."
 end
