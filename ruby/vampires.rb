36db8404fd041f391f150f0e23f69782e9e4e5da
Rachel Kerner <rachel.kerner@gmail.com>
2017-03-29 11:54:53 -0500

    Work on not a vampire methods

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -13,3 +13,26 @@ wantgarlic = gets.chomp
 puts "Would you like to enroll in the company's health insurance?"
 healthinsurance = gets.chomp
 
+def is_age_correct
+    if (2017 - birthtear) == age
+    correct_age = true
+    else 
+    correct_age = false
+    end
+end
+
+case is_a_vampire?
+    when correct_age == true
+        && when wantgarlic == "yes" 
+        && when healthinsurance == "yes"
+        puts "Probably not a vampire."
+    when correct_age == false
+        && when wantgarlic == "no",
+        && when healthinsurance == "no"
+        puts "Almost certainly a vampire"
+    when name == "Drake Cula"
+        || name == "Tu Fang"
+        puts "Definitely a vampire"
+    else 
+        puts "Results inconclusive."
+end
