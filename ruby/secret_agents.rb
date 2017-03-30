cdd388b5380a94ef4f8069fa4815b36365217230
Rachel Kerner <rachel.kerner@gmail.com>
2017-03-30 18:18:57 -0500

    Add pseudocode for encrpyt.

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,30 +1,40 @@
-def decrypt(str)
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-index = 0 
-while index < str.length
-  char = str[index]
-  str[index] = alphabet[alphabet.index(char)-1]
-  index += 1 
-end
-puts "#{str}"
-return str 
-end 
+# Encrypt Pseudocode
+# 1. Set index variable counter to 0 so it will match index
+# 2. Create a loop that runs for each letter in the string
+#   a. take the input for the first index spot and use .next to get the following character
+# 3. increase the index count to get to the next character in the string
+# 4. repeat until end of string
+# 5. Print the final result
+
 
 def encrypt(str)
-alphabet = "abcdefghijklmnopqrstuvwxyz"
 index = 0 
 while index < str.length
-  char = str[index]
-  if char == "z"
+  if str[index] == "z"
     str[index] = "a"
   else 
-    str[index] = alphabet[alphabet.index(char)+1]
+    str[index] = str[index].next
   end 
   index += 1 
 end
 puts "#{str}"
 return str 
 end 
+
+
+
+def decrypt(str)
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+index = 0 
+while index < str.length
+  str[index] = alphabet[alphabet.index(str[index])-1]
+  index += 1 
+end
+puts "#{str}"
+return str 
+end 
+
+
 
 # def encrypt(str)
 # index = 0 
