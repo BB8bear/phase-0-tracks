0379bda7d557a9b60166f06d73b22a1047ae6c25
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-03 09:26:47 -0500

    Add solves for remainder of assignment

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -41,6 +41,9 @@
 # => "the usual suspects"
 " world".insert(0, "hello")
 => "hello world"
+# -or-
+" suspects".prepend("the usual")
+=> "the usual suspects"
 
 "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
@@ -49,15 +52,23 @@
 "x".chop
 # => ""
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter"[1 .. -1]
 # => "he mystery of the missing first letter"
+"The mystery of the missing first two letters"[2 .. -1]
+# => "e mystery of the missing first two letters"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
+"Cats    like    to    nap.".squeeze(" ")
+# => "Cats like to nap."
 
-# "z".<???>
+"z".bytes
 # => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+# (What is the significance of the number 122 in relation to the character z?) - returns an array of bytes in the str "z"
+"a".bytes
+# => 97
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4
+"How many times does the letter 'b' appear in this string?".count("b")
+# => 1
