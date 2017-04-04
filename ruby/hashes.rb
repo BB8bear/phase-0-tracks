064ecb5d4dba6bd0690e29e521b735d6a8ca5575
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-04 10:57:12 -0500

    Final test of code, clean up pseudocode.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -13,6 +13,9 @@
 # 11. Add color preference to hash
 # 12. Print hash back to user once all questions have been answered
 # 13. Ask user if they need to update a key
+#   a. If key needs to be updated, ask user which key to update.
+#       - Ask user what they want to update key value with
+#   b. If key does not need to be updated, print has back to user.
 # 14. Print hash back to user once all questions have been answered and updates are done
 
 client_preferences = {
@@ -25,30 +28,34 @@ client_preferences = {
 
 puts "Please enter the following client details."
 puts "Client name:"
-:name = gets.chomp.to_str
+client_preferences[:name] = gets.chomp.to_str
 puts "Client age:"
-:age = gets.chomp.to_i
+client_preferences[:age] = gets.chomp.to_i
 puts "Number of client children:"
-:num_children = gets.chomp.to_i
+client_preferences[:num_children] = gets.chomp.to_i
 puts "Decor theme:"
-:decor_theme = gets.chomp.to_str
+client_preferences[:decor_theme] = gets.chomp.to_str
 puts "Least favorite color:"
-:avoid_color = gets.chomp.to_str
+client_preferences[:avoid_color] = gets.chomp.to_str
 
 p client_preferences
 
 puts "Do you need to update a key?"
-input = gets.chomp.to_sym
+input = gets.chomp
 
 if input == "none"
     p client_preferences
-else 
-    if client_preferences.include?(input) == true
-        puts "What would you like to update #{input} with?"
-        client_preferences[input] = gets.chomp.to_str
+elsif input == "yes" 
+    puts "Please enter which key you would like to update."
+    key = gets.chomp.to_sym
+    if client_preferences.include?(key) == true
+        puts "What would you like to update #{key} with?"
+        client_preferences[key] = gets.chomp.to_str
     else 
         puts "Error, key not found."
     end
+else
+    puts "Error, invalid answer."
 end
 
 p client_preferences
