018c5b361131a904624440d2630871c3f8af050b
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-04 15:42:16 -0500

    Add solves for 5.4 nested data module

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,49 @@
+closet = {
+    hanging_rack: {
+        hangers: [
+            "dresses",
+            "dress shirts",
+            "pants",
+            "blouses"
+        ],
+        garment_bags: [
+            "suit",
+            "dress pants",
+            "vests"
+        ]
+    },
+    shoe_rack: [
+        "black slip-ons",
+        "snow boots",
+        "sandals",
+        "sneakers",
+        "heels"
+    ],
+    storage_bins: {
+        tshirts: [ 
+            "space t-shirts",
+            "gamer t-shirts",
+            "solid t-shirts"
+        ],
+        scarves: [
+            "solid",
+            "floral",
+            "plaid",
+            "wool"
+            ],
+        hats: [
+            "top hat",
+            "fedora",
+            "baseball hat",
+            "winter cap",
+            "beret"
+        ]
+    }
+}
+
+p closet
+p closet[:storage_bins][:hats]
+p closet[:storage_bins][:scarves][2]
+closet[:hanging_rack][:hangers].push("jackets")
+p closet
+p closet[:hanging_rack][:hangers][-1]
