83dddb6645b39d6c7180f6503b2ddc6a11a726b6
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-05 13:10:10 -0500

    Add solves for Do the Thing

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,4 +6,57 @@ animal_babies = {
     "Goat" => "Kid"
 }
 
+cereal.each do |i| 
+    puts "I like to eat #{i} for breakfast."
+end
 
+cereal.map! do |i|
+    puts i.upcase
+end
+
+animal_babies.each do |parent, baby|
+    puts "A #{parent} has a #{baby}."
+end
+
+DTT = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+DTT.drop_while do |i| 
+    i < 5
+end 
+
+DTT.keep_if do |i|
+    i < 5
+end
+
+DTT.select { |i|
+    i.even?
+}
+
+DTT.delete_if do |i|
+    i >= 8
+end
+
+DTT = {
+    "Cat" => "Kitten",
+    "Dog" => "Puppy",
+    "Goat" => "Kid",
+    "Kangaroo" => "Joey"
+}
+
+DTT.delete_if { |animal, baby| 
+    animal >= "Goat"
+}
+
+p DTT
+
+DTT.keep_if { |animal, baby|
+    baby == "Kid"
+}
+
+p DTT
+
+DTT.reject { |animal, baby|
+    animal < "Dog"
+}
+
+p DTT
