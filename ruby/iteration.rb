a590f9ccd27057d1502325f0423013d23baa6925
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-05 18:22:06 -0500

    Add comments for ease of reading.

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,52 +1,65 @@
 
-# Release 1 Do The Thing
+# Release 0 - Write Your Own Method That Takes a Block
 def this_method
   puts "Hello how are you?"
-
  yield("Sucky")
 end
+
 this_method { |response| puts "#{response} ...that's too bad!" }
 
 
 # Release 2 Do The Thing
+
+# 1. Array
 cereal = ['Frosted Flakes', 'Cheerios', 'Pops', 'Wheaties']
 
+# Hash
 animal_babies = {
     "Cat" => "Kitten",
     "Dog" => "Puppy",
     "Goat" => "Kid"
 }
 
+# 2. Iterate through array using .each
 cereal.each do |i| 
     puts "I like to eat #{i} for breakfast."
 end
 
-cereal.map! do |i|
-    puts i.upcase
-end
-
+# Iterate through hash using each
 animal_babies.each do |parent, baby|
     puts "A #{parent} has a #{baby}."
 end
 
+# Iterate through array using map!
+cereal.map! do |i|
+    puts i.upcase
+end
+
+# Release 2 Do The Thing
+
+# Array for release 2
 DTT = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-DTT.drop_while do |i| 
+# 1. A method that iterates through the items and deletes any that meet a condition, less than 5
+DTT.delete_if do |i|
     i < 5
-end 
+end
 
+# 2. A method that filters for items that do satisfy a certain condition, less than 5
 DTT.keep_if do |i|
     i < 5
 end
 
+# 3. A different method that filters for items satisfying a certain condition, if even
 DTT.select { |i|
     i.even?
 }
 
-DTT.delete_if do |i|
-    i >= 8
-end
+# 4. A method that will remove items until condition evaluates false, then stops
+DTT.drop_while do |i|
+    i < 5
 
+# Hash for release 2
 DTT = {
     "Cat" => "Kitten",
     "Dog" => "Puppy",
@@ -54,20 +67,17 @@ DTT = {
     "Kangaroo" => "Joey"
 }
 
+# 1. A method that iterates through the items and deletes any that meet a condition, if animal is before Goat in the hash
 DTT.delete_if { |animal, baby| 
     animal >= "Goat"
 }
 
-p DTT
-
+# 2. A method that filters for items that do satisfy a certain condition, if value is Kid
 DTT.keep_if { |animal, baby|
     baby == "Kid"
 }
 
-p DTT
-
+# 3. A different method that filters for items satisfying a certain condition, remove anything before Dog key in the hash
 DTT.reject { |animal, baby|
     animal < "Dog"
 }
-
-p DTT
