1362b3cd342c92c635c10a5034983d750fcba87e
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-06 17:28:19 -0500

    Clean up comments

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -9,45 +9,24 @@
 #     - map consonants to next consonant
 #     - join letters back together
 
-realname = "Rachel Kerner"
 
-
-# Swap first and last name
-realname.split(" ").reverse
-
-# Replace vowel with next vowel
-def vowel_rotate(name)
-    vowels = ['a', 'e', 'i', 'o', 'u']
-
-    name = name.split('')
-    name_vrotate = name.map { |char|
-        if vowels.include?(char)
-            vowels.rotate(1)[vowels.index(char)]
-        else
-            char
-        end
-    }
-    name_vrotate.join
+def name_rotate(name)
+  consonants = %w[b c d f g h j k l m n p q r s t v w x y z]
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  
+  p name = name.downcase.split('')
+  
+  name_rotate = name.map { |char|
+    if vowels.include?(char)
+      vowels.rotate(1)[vowels.index(char)]
+    elsif consonants.include?(char)
+      consonants.rotate(1)[consonants.index(char)]
+    else
+      char
+    end
+  }
+  
+  name_rotate.join.split(' ').reverse.map { |x| x.capitalize }.join(' ')
 end
 
-p vowel_rotate "aeiouyz"
-
-# Replace consonants with next consonant
-def consonant_rotate(name)
-    consonants = %w[b c d f g h j k l m n p q r s t v w x y z]
-    capital_consonants =%w[B C D F G H J K L M N P Q R S T V W X Y Z]
-
-    name = name.downcase.split('')
-
-    name_crotate = name.map { |char|
-        if consonants.include?(char)
-            consonants.rotate(1)[consonants.index(char)]
-        else
-            char           
-        end
-    }
-    agent_name = name_crotate.join
-    agent_name.split.map{ |x| x.capitalize}.join(' ')
-end
-
-p consonant_rotate "Rachel Kerner"
+p name_rotate "Felicia Torres"
