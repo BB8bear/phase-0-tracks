89f9f094c957b6b6a8e39b2eaae6a383c69a7ce9
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-09 10:41:57 -0500

    Add release 2 functionality to alias manager, display name and corresponding alias.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -14,7 +14,7 @@ def name_rotate(name)
   consonants = %w[b c d f g h j k l m n p q r s t v w x y z]
   vowels = ['a', 'e', 'i', 'o', 'u']
   
-  p name = name.downcase.split('')
+  name = name.downcase.split('')
   
   name_rotate = name.map { |char|
     if vowels.include?(char)
@@ -29,4 +29,21 @@ def name_rotate(name)
   name_rotate.join.split(' ').reverse.map { |x| x.capitalize }.join(' ')
 end
 
-p name_rotate "Felicia Torres"
+# p name_rotate "Felicia Torres"
+
+puts "Please enter a name to get a fake name back. Enter 'quit' to exit the program."
+name = gets.chomp.to_s
+alias_array = Hash.new
+
+while name != 'quit'
+    puts name_rotate(name)
+    alias_array[name] = name_rotate(name)
+    puts "Please enter a name to get a fake name back. Enter 'quit' to exit the program."
+    name = gets.chomp.to_s
+end 
+
+alias_array.each do 
+  |n, a| puts "#{a} is actually #{n}"
+end 
+
+
