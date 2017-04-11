54e202b0d683456bc8ac5e7f4fd28bef1fd58e18
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-11 16:32:38 -0500

    Complete release 2.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,56 +1,58 @@
 class Santa
-  def initialize(gender, ethnicity, age)
-    p "Initializing Santa instance..."
-    @gender = gender
-    @ethnicity = ethnicity
-    @age = age
-    @reindeer_ranking = ['Rudolph', 'Dasher', 'Dancer', 'Prancer', 'Vixen', 'Comet', 'Cupid', 'Donner', 'Blitzen']
-  end  
+    attr_reader :ethnicity, :reindeer_ranking
+    attr_accessor :age, :gender
 
-  def speak
-    p "Ho, ho, ho! Haaaappy holidays!"
-  end
+    def initialize(gender, ethnicity, age)
+        p "Initializing Santa instance..."
+        @gender = gender
+        @ethnicity = ethnicity
+        @age = age
+        @reindeer_ranking = ['Rudolph', 'Dasher', 'Dancer', 'Prancer', 'Vixen', 'Comet', 'Cupid', 'Donner', 'Blitzen']
+    end  
 
-  def eat_milk_and_cookies(cookie_type)
-    p "That was a good #{cookie_type} cookie!"
-  end
+    def speak
+        p "Ho, ho, ho! Haaaappy holidays!"
+    end
 
-  def celebrate_birthday
-    @age + 1
-  end
+    def eat_milk_and_cookies(cookie_type)
+        p "That was a good #{cookie_type} cookie!"
+    end
 
-  def gets_mad_at(reindeer_name)
-    reindeer_ranking.delete(reindeer_name)
-    reindeer_ranking << reindeer_name
-  end
+    def celebrate_birthday
+        @age + 1
+    end
 
-  def reindeer_ranking
-    return @reindeer_ranking
-  end
+    def gets_mad_at(reindeer_name)
+        reindeer_ranking.delete(reindeer_name)
+        reindeer_ranking << reindeer_name
+    end
 
-  def gender 
-    @gender
-  end
+# # getter methods for attributes
+#     def gender 
+#         @gender
+#     end
+
+#     def age 
+#         @age 
+#     end
+
+#     def ethnicity 
+#         @ethnicity 
+#     end
+
+#     def reindeer_ranking
+#         @reindeer_ranking 
+#     end
+
+#     # setter methods for attributes
+#     def gender=(new_gender)
+#         @gender = new_gender
+#     end
+
+#     def reindeer_ranking=(new_reindeer_ranking)
+#         @reindeer_ranking = new_reindeer_ranking
+#     end
   
-  def age 
-    @age 
-  end
-  
-  def ethnicity 
-    @ethnicity 
-  end
-
-  def gender=(new_gender)
-    @gender = new_gender
-  end
-  
-  def reindeer_ranking
-    @reindeer_ranking 
-  end
-  
-  def reindeer_ranking=(new_reindeer_ranking)
-    @reindeer_ranking = new_reindeer_ranking
-  end
 end
 
 santas = []
@@ -58,6 +60,7 @@ genderArray = ['agender', 'female', 'bigender', 'male', 'gender fluid', 'transge
 ethnicityArray = ['Black', 'Latino', 'White', 'Pacific Islander', 'Native American', 'Alaska Native']
 ageArray = [30, 21, 18, 7, 50, 87]
 
+# Tests santa array initialization
 puts "Testing Santas array..."
 puts " "
 genderArray.length.times do |i|
@@ -67,18 +70,26 @@ genderArray.length.times do |i|
     else 
       puts "There are now #{santas.length} Santa instances in the array."
     end
-    
-    puts "Santa instance ##{santas.length} is a #{ethnicityArray[i]} #{genderArray[i]} #{ageArray[i]} years old."
-    puts " "
-    puts "-----"
-    puts " "
 end
 
+# Prints each santa's instance number, gender, ethnicity, and age.
+santas.length.times do |i|
+  puts "Santa instance ##{santas.length} is a #{santas[i].gender} #{santas[i].ethnicity} who is #{santas[i].age} years old."
+end
 
+# Tests reindeer_ranking functionality
 santa = Santa.new('agender', 'Black', '30')
 santa.gets_mad_at('Dasher')
-puts "Thr #{santa.gender} #{santa.ethnicity} #{santa.age} year old Santa gets mad at #{santa.reindeer_ranking[-1]}"
-# p santa.reindeer_ranking
+puts "The #{santa.gender} #{santa.ethnicity} #{santa.age} year old Santa gets mad at #{santa.reindeer_ranking[-1]}"
 
-puts santa.age
-puts santa.ethnicity
+
+# Tests getter methods
+santas.length.times do |i|
+    puts santas[i].age
+    puts santas[i].ethnicity
+    puts santas[i].reindeer_ranking
+end
+
+santas.length.times do |i|
+  puts "The #{santas[i].gender} #{santas[i].ethnicity} #{santas[i].age} year old Santa turns #{santas[i].celebrate_birthday} next year."
+end
