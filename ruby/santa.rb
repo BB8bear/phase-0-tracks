dab46d57d4b58f5f101b94fba007f6828df7f0fe
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-11 13:42:40 -0500

    Add instance variables for gender, ethnicity, age.

---
+++ ruby/santa.rb
@@ -0,0 +1,36 @@
+class Santa
+  def initialize(name)
+    p "Initializing Santa instance..."
+    @name = name
+    @location = "the North Pole"
+    @gender = gender
+    @ethnicity = ethnicity
+    # @reindeer_ranking = ['Rudolph', 'Dasher', 'Dancer', 'Prancer', 'Vixen', 'Comet', 'Cupid', 'Donner', 'Blitzen']
+    @age = 0
+  end  
+
+  def speak
+    p "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie_type)
+    p "That was a good #{cookie_type} cookie!"
+  end
+end
+
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+
