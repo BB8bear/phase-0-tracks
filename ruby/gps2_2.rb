8f91fa8e121c91e5458f0e5c498bf62636196380
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-13 17:17:46 -0500

    Add pseudocode for method to create a list.

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,10 +1,11 @@
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
 # steps: 
-  # [fill in any steps here]
-  # set default quantity
-  # print the list to the console [can you use one of your other methods here?]
-# output: [what data type goes here, array or hash?]
+  # 1. Create an empty hash and define it
+  # 2. Add items to the hash from the list
+  # 3. Set default quantity for all items in the list to 0
+  # 4. Print the list to the console with Print a List method below
+# output: Hash
 
 # Method to add an item to a list
 # input: list, item name, and optional quantity
