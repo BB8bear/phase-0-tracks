db61cb8fcba084043af6ed517ec7d30ce2c02479
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-13 11:02:08 -0500

    Add yell_happily method and driver code to test functionality.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,17 @@
-module Shout
-    # code will go here
+m#Release 1
+
+odule Shout
+    # Method for yelling angrily
+    def self.yell_angrily(words)
+        words + "!!!" + " :("
+    end
+
+    # Method for yelling happily
+    def self.yell_happily(words)
+        words + "!!!" + " :)"
+    end
+
 end
+
+p Shout.yell_happily("I love cats")
+p Shout.yell_angrily("I hate spiders")
