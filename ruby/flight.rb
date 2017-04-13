4a48fbfab7ebbb4392e8cfc100913508315ab878
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-13 11:06:29 -0500

    Add take off method to flight module and include in bird and plane classes.

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -1,11 +1,24 @@
 # Release 2
 
+# Flight module
+module Flight
+
+    # Take off method
+    def take_off(altitude)
+        puts "Taking off and ascending until reaching #{altitude} ..."
+    end
+end
+
+
+
 # Bird class
 class Bird
+    include Flight
 end
 
 # Plane class
 class Plane
+    include Flight
 end
 
 
