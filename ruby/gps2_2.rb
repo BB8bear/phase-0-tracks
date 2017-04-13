bff49f3d993349211995ab07634a942dcf74d85a
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-13 17:26:42 -0500

    Finalize pseudocode for all methods.

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -9,20 +9,31 @@
 
 # Method to add an item to a list
 # input: list, item name, and optional quantity
-# steps:
-# output:
+# steps: 
+  # 1. Add new key and value to the hash
+  # 2. Print the list to the console with the Print a List method below
+# output: Hash with updated keys and values
 
 # Method to remove an item from the list
-# input:
+# input: list, item name
 # steps:
-# output:
+  # 1. Find the item in the list by key
+  # 2. Delete the key from the hash (quantity will be removed along with the key)
+  # 3. Print the list to the console with the Print a List method below
+# output: Hash with remaining keys and values
 
 # Method to update the quantity of an item
-# input:
+# input: list, item name, and quantity to update
 # steps:
-# output:
+  # 1. Find the item in the list by key
+  # 2. Reassign value by accessing key(item)
+  # 3. Print the list to the console with the Print a List method below
+# output: Hash with updated keys and values
 
 # Method to print a list and make it look pretty
-# input:
-# steps:
-# output:
+# input: list
+# steps: 
+  # 1. Give list a title "Grocery List"
+  # 2. Add padding space below the title " "
+  # 3. Iterate over the list hash for each item "Item qty 1."
+# output: Printed list using the hash values
