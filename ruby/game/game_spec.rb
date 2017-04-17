5e949cece0a2b5fce93873725e5d7d78f5e74bac
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-17 07:53:01 -0500

    Add check for valid guess, check for end.

---
+++ ruby/game/game_spec.rb
@@ -0,0 +1,24 @@
+require_relative 'game'
+
+describe WordguessGame do 
+    let(:game) {WordguessGame.new("unicorn")}
+    
+    it "stores the input word" do
+        expect(game.word).to eq "unicorn"
+    end
+
+    it "initializes word output display" do
+        game.create_output
+        expect(game.word_output).to eq "_______"
+    end
+
+    it "checks for game end" do
+        game.word_output = "unicorn"
+        expect(game.check_for_end).to be_truthy
+    end
+
+    it "checks guess against word for each instance of guess in word" do
+        expect(game.valid_guess("u")).to be_truthy
+    end 
+end
+    
