d79554f47a0898f9d417290761fa21c0698743ea
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-16 21:08:42 -0500

    Pseudocode for word game challenge.

---
+++ ruby/word_game/word_game.rb
@@ -0,0 +1,17 @@
+# game Class
+
+# Stores input word as a variable
+# Split word on each character into component letters
+# Store character breakdown in an array
+# Another user must guess the correct letters in word in (length + 5) guesses
+# Any repeated letter guesses do not count
+    # Store all guesses in another array
+    # Check new input against guess array
+# Output feedback after each guess showing which letters have been guessed in which spaces
+
+# Class must have variables for
+# word storage - read
+# word character array - read
+# guess character array - read/write
+# store number of guesses - read/write
+
