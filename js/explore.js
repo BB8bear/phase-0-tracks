84edb581d7a6e2a10c9239a51dd4f65348ef2beb
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-18 12:50:01 -0500

    Complete loop and return for reverseString function.

--- js/explore.js
+++ js/explore.js
@@ -1,9 +1,22 @@
 // Reverse String
 
 // Declare string variable
-// For each character in the string, find the index
-// Take the last index in the source string
-// Push it into a new string at the beginning
+// Find the string length. 
+// Assign the index variable to the last place in the string.
+// Iterate through the string backward based on index.
 // Repeat until source string has been iterated through once
 // Print the new string containing the reversed word
 
+// var string = "hello";
+// var first = string.charAt(string.length-1);
+// console.log(first);
+var newString = "";
+
+function reverseString(string) {
+    for (var i = string.length-1; i >= 0; i -= 1) {
+        newString += string.charAt(i);
+    }
+    return(newString);    
+}
+
+console.log(reverseString("hello"));
