e418b557efb708971d1e5b7ce254f7427df15c0c
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-18 11:50:25 -0500

    Write pseudocode for string reversal program.

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// Reverse String
+
+// Declare string variable
+// For each character in the string, find the index
+// Take the last index in the source string
+// Push it into a new string at the beginning
+// Repeat until source string has been iterated through once
+// Print the new string containing the reversed word
+
