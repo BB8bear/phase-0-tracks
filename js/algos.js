75f8602be911da898f5bd314440903d5ce7c5799
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-19 14:39:27 -0500

    Write pseudocode for Release 0 problem

---
+++ js/algos.js
@@ -0,0 +1,21 @@
+// Release 0: Find the Longest Phrase
+// Create an array variable that stores the words to check.
+// Set a variable to store the current biggest string
+// Create a loop that iterates over each item in the array.
+    // Create a variable to stand for the array item index
+    // Set the index variable to 0
+    // Run the loop until the array's length matches index
+// Compare value in biggest string variable to current array value
+// If current array value is larger than biggest string variable value
+// Assign current array value to biggest string variable
+// Continue comparing the remainder of the array
+// Return the largest string stored in variable
+
+
+
+
+
+var animals = ["kitten", "dog", "bunny", "bird", "giraffe"];
+
+
+
