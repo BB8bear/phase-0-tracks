bf8de29036d78e127980daf72739c63e0fed3ae2
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-19 20:20:35 -0500

    create function for doesItMatch, create test cases and execute.

--- js/algos.js
+++ js/algos.js
@@ -39,5 +39,25 @@ console.log(findBiggestString(animals));
 // If there is a match, object2 has a matching key and value because you could find the value by key
 // otherwise return false after checking entire loop
 
+var object1 = {name: "Steven", age: 54};
+var object2 = {name: "Tamir", age: 54};
+var object3 = {name: "Jimmy Dean", age: 12};
+var object4 = {height: 34, weight: 17};
+
+function doesItMatch(item1, item2) { //Create a function that takes two object variables
+  
+  for (var key in item1) {
+    if (item1[key] === item2[key]) {
+      return true;
+    } 
+  }
+  return false;
+}
+
+// console.log(doesItMatch(object1, object2));
+// console.log(doesItMatch(object1, object3));
+// console.log(doesItMatch(object1, object4));
+
+
 
 
