c0320cde1fbf1feeebf1662c6339ed56d4565239
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-24 09:46:38 -0500

    create code to use two arrays to create an object

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,6 +1,22 @@
+// Release 0
+
 var colors = ["red", "green", "blue", "orange"];
 var horses = ["Ed", "Sunny", "Mittens", "Pacer"];
 
 colors.push("yellow");
 horses.push("Diamond");
+
+// Release 1
+
+function Horse(name, color) {
+    name = name;
+    color = color;
+}
+
+for (var i = 0; i < colors.length; i++) {
+    var newHorse = new Horse(horses[i], colors[i]);
+    console.log(newHorse);
+}
+
+// Release 2
 
