5cd9f87c311e7fe40e1cb733acd332ee1f3fc59a
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-24 10:22:35 -0500

    create constructor for new cars

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,22 +1,40 @@
 // Release 0
 
 var colors = ["red", "green", "blue", "orange"];
-var horses = ["Ed", "Sunny", "Mittens", "Pacer"];
+var names = ["Ed", "Sunny", "Mittens", "Pacer"];
 
 colors.push("yellow");
-horses.push("Diamond");
+names.push("Diamond");
 
 // Release 1
 
-function Horse(name, color) {
-    name = name;
-    color = color;
+var horses = {};
+
+for (var i = 0; i < colors.length; i ++) {
+    horses[names[i]] = colors[i];
 }
 
-for (var i = 0; i < colors.length; i++) {
-    var newHorse = new Horse(horses[i], colors[i]);
-    console.log(newHorse);
-}
+console.log(horses);
 
 // Release 2
+
+function Car(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+
+    this.vroom = function() {
+        console.log("Vroom vroom!");
+    };
+}
+
+var newCar = new Car("Honda", "Civic", 2005);
+console.log(newCar);
+newCar.vroom();
+
+var anotherCar = new Car("Ford", "F-150", 2014);
+console.log(anotherCar);
+anotherCar.vroom();
+
+// Release 3
 
