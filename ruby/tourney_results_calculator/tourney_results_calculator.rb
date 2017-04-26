f1a9830d359d66ee5b3b3769a43e32302627d934
Rachel Kerner <rachel.kerner@gmail.com>
2017-04-26 15:41:58 -0500

    Create test data for program

--- ruby/tourney_results_calculator/tourney_results_calculator.rb
+++ ruby/tourney_results_calculator/tourney_results_calculator.rb
@@ -4,36 +4,48 @@ require 'faker'
 
 # create SQLite3 database
 db = SQLite3::Database.new("tourney.db")
+db.results_as_hash = true
 
 # learn about fancy string delimiters
-# create_table_cmd = <<-SQL
-#   CREATE TABLE IF NOT EXISTS tourney(
-#     id INTEGER PRIMARY KEY,
-#     name VARCHAR(255),
-#     1 VARCHAR(255),
-#     2 VARCHAR(255),
-#     3 VARCHAR(255),
-#     4 VARCHAR(255),
-#     5 VARCHAR(255),
-#     6 VARCHAR(255),
-#     7 VARCHAR(255),
-#     8 VARCHAR(255),
-#     9 VARCHAR(255), 
-#     10 VARCHAR(255),
-#     Wins VARCHAR(255),
-#     Losses VARCHAR(255),
-#     Place VARCHAR(255)
-#   )
-# SQL
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS tourney(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    "1" VARCHAR(255),
+    "2" VARCHAR(255),
+    "3" VARCHAR(255),
+    "4" VARCHAR(255),
+    "5" VARCHAR(255),
+    "6" VARCHAR(255),
+    "7" VARCHAR(255),
+    "8" VARCHAR(255),
+    "9" VARCHAR(255), 
+    "10" VARCHAR(255),
+    Wins VARCHAR(255),
+    Losses VARCHAR(255),
+    Place VARCHAR(255)
+  )
+SQL
 
 # create a table (if it's not there already)
-# db.execute(create_table_cmd)
+db.execute(create_table_cmd)
 
 # add a test entrant
 # db.execute("INSERT INTO tourney (name) VALUES ('Bob')")
 
-# retrievie data
-# tourney = db.execute("SELECT * FROM tourney")
-# tourney.each do |entrant|
-#  puts "#{entrant['name']} is in the tourney."
+# add test case entrants 
+# def create_entrant(db, name)
+#   db.execute("INSERT INTO tourney (name) VALUES (?)", [name])
 # end
+
+# 10.times do
+#   create_entrant(db, Faker::Name.name)
+# end
+
+# retrievie data
+tourney = db.execute("SELECT * FROM tourney")
+
+tourney.each do |entrant|
+ puts "#{entrant[1]} is in the tourney."
+end
+
