97f350764e4c612210985f97a772b39dd0e31d21
Rachel Kerner <rachel.kerner@gmail.com>
2017-05-03 14:22:05 -0500

    Complete release 0 of 9.4

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -1,5 +1,6 @@
 # require gems
 require 'sinatra'
+require 'sinatra/reloader'
 require 'sqlite3'
 
 db = SQLite3::Database.new("students.db")
@@ -8,9 +9,9 @@ db.results_as_hash = true
 # write a basic GET route
 # add a query parameter
 # GET /
-get '/' do
-  "#{params[:name]} is #{params[:age]} years old."
-end
+# get '/' do
+#   "#{params[:name]} is #{params[:age]} years old."
+# end
 
 # write a GET route with
 # route parameters
@@ -42,5 +43,52 @@ end
 
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
+  "Hello world"
   student.to_s
 end
+
+# 1. write a GET route that displays
+# an address
+
+get '/contact' do
+  "Chicago's Dev Bootcamp campus is located at 1033 W Van Buren St, Chicago, IL 60607."
+end
+
+# 2. write a GET route that takes
+# a person's name as a query parameter
+# and say "Good job, [name]!" 
+# If the query parameter is not present, just say 
+# "Good job!"
+
+get '/' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+
+# 3. a route that uses route parameters to add two numbers
+# and respond with the result
+
+get '/:number_1/add/:number_2' do
+  "Hello world"
+  result = params[:number_1].to_i + params[:number_2].to_i
+  result
+  "#{params[:number_1]} plus #{params[:number_2]} equals #{result}."
+end
+
+# 4. write a route that allows the user to search the Database
+# find student by name
+
+get '/students/' do 
+  student = db.execute("SELECT * FROM students WHERE name=?", [params[:name]])[0]
+  response = ""
+  response << "ID: #{student['id']}<br>"
+  response << "Name: #{student['name']}<br>"
+  response << "Age: #{student['age']}<br>"
+  response << "Campus: #{student['campus']}<br><br>"
+  response
+end
+
